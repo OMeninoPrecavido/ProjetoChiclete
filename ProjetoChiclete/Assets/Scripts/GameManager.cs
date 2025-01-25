35b8efa85ee75274cf9c5a9c705de15278939975
OMeninoPrecavido <97351634+OMeninoPrecavido@users.noreply.github.com>
2025-01-25 09:10:19 -0300

    Timer e pontos

--- ProjetoChiclete/Assets/Scripts/GameManager.cs
+++ ProjetoChiclete/Assets/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
+using System.Collections;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 //Game manager class
 public class GameManager : MonoBehaviour
 {
-    //References to other UI panels
+    //References to other UI elements
     [SerializeField] GameObject arrowsUi; //Center arrows UI
     [SerializeField] GameObject gumChooserUi; //Side gum options UI
+    [SerializeField] TextMeshProUGUI timerUi; //Game timer UI
+    [SerializeField] TextMeshProUGUI pointsUi; //Game points UI
 
     public static GameManager instance; //SINGLETON instance
 
     public GameState gameState; //Variable that defines the current state of the game
+
+    public int Timer { get; private set; }
+    private const int GAME_TIME_SECS = 90;
+
+    public int Points { get; private set; }
 
     private void Awake()
     {
@@ -25,6 +34,16 @@ public class GameManager : MonoBehaviour
             SetGameState(GameState.GumSelection);
         }
 
+        timerUi.text = GAME_TIME_SECS.ToString();
+        Timer = GAME_TIME_SECS;
+
+        Points = 0;
+        pointsUi.text = Points.ToString();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(DecreaseTimer());
     }
 
     //Configures UI for the GumSelection game state
@@ -53,6 +72,21 @@ public class GameManager : MonoBehaviour
             case GameState.ArrowSequence:
                 ArrowSequence();
                 break;
+        }
+    }
+
+    public void IncreasePoints(int value)
+    {
+        Points += value;
+    }
+
+    IEnumerator DecreaseTimer()
+    {
+        while (Timer > 0)
+        {
+            Timer--;
+            timerUi.text = Timer.ToString();
+            yield return new WaitForSeconds(1);
         }
     }
 }
