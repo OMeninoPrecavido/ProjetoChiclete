acba6c86a3810cfe42de733e9d14df1f4b75da4b
OMeninoPrecavido <97351634+OMeninoPrecavido@users.noreply.github.com>
2025-01-25 13:08:01 -0300

    GumPrefab mais bonito

--- ProjetoChiclete/Assets/Scripts/GumObject.cs
+++ ProjetoChiclete/Assets/Scripts/GumObject.cs
@@ -4,35 +4,52 @@ using UnityEngine.UI;
 //Class representative of the gum icons showing up on screen
 public class GumObject : MonoBehaviour
 {
+    [SerializeField] Image TuttiBallon;
+    [SerializeField] Image BBBallon;
+    [SerializeField] Image MintBallon;
+
+    [SerializeField] Image TuttiGum;
+    [SerializeField] Image BBGum;
+    [SerializeField] Image MintGum;
+
     public Gum gum { get; private set; } //Gum represented by the GumObject
+
     void Awake()
     {
         //Initializes Gum field and changes the image's color
         gum = new Gum();
-        ChangeSpriteColor();
+        ChangeSpriteByFlavour();
     }
 
     //Method that allows other classes to define the Gum field on this class
     public void SetGum(Gum gum)
     {
         this.gum = gum;
-        ChangeSpriteColor();
+        ChangeSpriteByFlavour();
     }
 
     //Changes color of sprite based on the gum flavour
-    private void ChangeSpriteColor()
+    private void ChangeSpriteByFlavour()
     {
-        Image image = this.GetComponent<Image>(); //Image component attached to the same object this script is attached to
+        MintBallon.gameObject.SetActive(false);
+        MintGum.gameObject.SetActive(false);
+        BBBallon.gameObject.SetActive(false);
+        BBGum.gameObject.SetActive(false);
+        TuttiBallon.gameObject.SetActive(false);
+        TuttiGum.gameObject.SetActive(false);
         switch (gum.flavour)
         {
             case Flavour.Mint:
-                image.color = Color.green;
+                MintBallon.gameObject.SetActive(true);
+                MintGum.gameObject.SetActive(true);
                 break;
             case Flavour.Tutti:
-                image.color = Color.magenta;
+                TuttiBallon.gameObject.SetActive(true);
+                TuttiGum.gameObject.SetActive(true);
                 break;
             case Flavour.Blueberry:
-                image.color = Color.blue;
+                BBBallon.gameObject.SetActive (true);
+                BBGum.gameObject.SetActive(true);
                 break;
         }
     }
