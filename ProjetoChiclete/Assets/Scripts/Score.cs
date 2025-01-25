541664d3e99ec14eea86e324ed33eb95d3500ac4
Murillo <murillomartins19@gmail.com>
2025-01-25 15:38:05 -0300

    Adicionando score para triplas

--- ProjetoChiclete/Assets/Scripts/Score.cs
+++ ProjetoChiclete/Assets/Scripts/Score.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Score : MonoBehaviour
@@ -10,13 +11,34 @@ public class Score : MonoBehaviour
         {Difficulty.Hard, 5},
     };
 
-    public static int Calculate(List<Gum> gums) {
+    public static int Calculate(List<Gum> gums)
+    {
+        List<Difficulty> difficulties = gums.Select(item => item.difficulty).ToList();
+        List<Flavour> flavours = gums.Select(item => item.flavour).ToList();
         int finalScore = 0;
         //Calculate scores based on the individual difficulty
-        foreach (Gum gum in gums)
+        foreach (Difficulty difficulty in difficulties)
         {
-            finalScore += DifficultyModifier[gum.difficulty];
+            finalScore += DifficultyModifier[difficulty];
         }
+
+        //Getting sliding window to calculate for groups of 3
+        for (int i = 0; i < flavours.Count - 3; i++) {
+            List<Flavour> window = flavours.GetRange(i, 3);
+            //Check if we have a all different trio
+            if(window.Distinct().Count() == window.Count) {
+                i += 3;
+                continue;
+            }
+            //Check if we have a trio in a list
+            if(window.All(item => item == window[0]))
+            {
+                i += 3;
+                continue;
+            }
+            Debug.Log(string.Join(" ", window));
+        }
+
         return finalScore;
     }
 }
